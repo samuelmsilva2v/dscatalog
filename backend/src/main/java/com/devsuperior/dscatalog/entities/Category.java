5776046c14c55d743e32863fb81c9e387bf56946
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-05-25 13:23:20 -0300

    h2 database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,14 +2,27 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
+@Entity
+@Table(name = "tb_category")
 @Data
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	public Category() {
+		// TODO Auto-generated constructor stub
+	}
 
 	public Category(Long id, String name) {
 		super();
