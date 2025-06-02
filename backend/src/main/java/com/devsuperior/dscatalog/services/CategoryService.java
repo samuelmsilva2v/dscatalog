e3fe5d5c559298da2a93ad9885495b1289479329
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-06-02 16:07:45 -0300

    update category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -11,7 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dscatalog.dtos.CategoryDto;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -21,29 +23,42 @@ public class CategoryService {
 
 	@Transactional(readOnly = true)
 	public List<CategoryDto> findAll() {
-		
+
 		List<Category> list = categoryRepository.findAll();
-		
+
 		return list.stream().map(x -> new CategoryDto(x)).collect(Collectors.toList());
 	}
 
 	@Transactional(readOnly = true)
 	public CategoryDto findById(Long id) {
-		
+
 		Optional<Category> obj = categoryRepository.findById(id);
-		var entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
-		
+		var entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+
 		return new CategoryDto(entity);
 	}
 
 	@Transactional(readOnly = true)
 	public CategoryDto insert(CategoryDto dto) {
-		
+
 		var entity = new Category();
 		entity.setName(dto.getName());
-		
 		entity = categoryRepository.save(entity);
-		
+
 		return new CategoryDto(entity);
+	}
+
+	@Transactional
+	public CategoryDto update(Long id, CategoryDto dto) {
+		
+		try {
+			var entity = categoryRepository.getReferenceById(id);
+			entity.setName(dto.getName());
+			entity = categoryRepository.save(entity);
+			return new CategoryDto(entity);
+		} 
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id " + id + " not found");
+		}
 	}
 }
