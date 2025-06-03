fca511294734ef6997a9e3415056d52a8bb88738
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-06-03 17:35:07 -0300

    product entity sql script

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -29,10 +29,10 @@ public class CategoryResource {
 	private CategoryService categoryService;
 
 	@GetMapping
-	public ResponseEntity<Page<CategoryDto>> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+	public ResponseEntity<Page<CategoryDto>> findAll(@RequestParam(defaultValue = "0") Integer page,
+			@RequestParam(defaultValue = "12") Integer linesPerPage,
+			@RequestParam(defaultValue = "ASC") String direction,
+			@RequestParam(defaultValue = "name") String orderBy
 			) {
 		
 		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
