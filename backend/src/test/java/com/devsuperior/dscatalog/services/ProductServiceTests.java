71e1991fb0dc1762ae17f7d9b6822f700921ca3f
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-06-26 17:08:06 -0300

    find all tests

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -1,16 +1,27 @@
 package com.devsuperior.dscatalog.services;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.devsuperior.dscatalog.dtos.ProductDto;
+import com.devsuperior.dscatalog.entities.Product;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import com.devsuperior.dscatalog.tests.Factory;
 
 /*
  * Não carrega o contexto, mas permite usar os
@@ -36,6 +47,8 @@ public class ProductServiceTests {
 	private long existingId;
 	private long nonExistingId;
 	private long dependentId;
+	private PageImpl<Product> page;
+	private Product product;
 	
 	@BeforeEach
 	void setUp() throws Exception {
@@ -46,6 +59,8 @@ public class ProductServiceTests {
 		existingId = 1L;
 		nonExistingId = 2L;
 		dependentId = 3L;
+		product = Factory.createProduct();
+		page = new PageImpl<>(List.of(product));
 		
 		/*
 		 * Simula exceção de integridade referencial ao tentar deletar um "dependentId"
@@ -55,9 +70,24 @@ public class ProductServiceTests {
 		/*
 		 * Configura o comportamento do existsById para cada cenário 
 		 */
+		Mockito.when(productRepository.findAll((Pageable)ArgumentMatchers.any())).thenReturn(page);
+		Mockito.when(productRepository.save(ArgumentMatchers.any())).thenReturn(product);
+		Mockito.when(productRepository.findById(existingId)).thenReturn(Optional.of(product));
+		Mockito.when(productRepository.findById(nonExistingId)).thenReturn(Optional.empty());
 		Mockito.when(productRepository.existsById(existingId)).thenReturn(true);
 		Mockito.when(productRepository.existsById(nonExistingId)).thenReturn(false);
 		Mockito.when(productRepository.existsById(dependentId)).thenReturn(true);
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnPage() {
+		
+		var pageable = PageRequest.of(0, 10);
+		
+		Page<ProductDto> result = productService.findAllPaged(pageable);
+		
+		Assertions.assertNotNull(result);
+		Mockito.verify(productRepository, Mockito.times(1)).findAll(pageable);
 	}
 	
 	@Test
@@ -85,4 +115,6 @@ public class ProductServiceTests {
 		
 		Mockito.verify(productRepository, Mockito.times(1)).deleteById(existingId);
 	}
+	
+	
 }
